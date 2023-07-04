9761e6fe1d2ecdd28b3c95062e881030998cff5b
Raj Wadia <wadiaraj7@gmail.com>
2023-07-03 21:44:43 -0600

    added input field for genre.

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,41 +1,49 @@
+import { Checkbox, FormControlLabel, FormGroup } from '@mui/material';
 import React, { useContext, useEffect, useRef, useState } from "react";
 import axios from '../api/axios.js';
 import AuthContext from "../context/AuthProvider.jsx";
 import './form.css';
-import { FormControlLabel } from '@mui/material';
-import { FormGroup } from '@mui/material';
-import { Checkbox } from '@mui/material';
 
 function Register(props) {
 
   const REGISTER_URL = 'http://localhost:3001/register';
-  
+
   const errRef = useRef();
   const userRef = useRef();
 
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [pass, setPass] = useState("");
-  const [art, setArt] = useState(false);
+  const [artistChecked, setArtistChecked] = useState();
   const [err, setErr] = useState('');
   const [showAlert, setShowAlert] = useState(false);
+  const [genre, setGenre] = useState('');
 
   useEffect(() => {
     setErr('');
-  }, [email, pass, name]);
+  }, [email, pass, name, genre]);
 
   useEffect(() => {
     userRef.current.focus();
   }, []);
-  
+
+  const handleChange = (e) => {
+    setArtistChecked(e.target.checked);
+    document.getElementById('genre').style.display = 'inline';
+    if (!e.target.checked) document.getElementById('genre').style.display = 'none';
+  }
   const handleSubmit = async (e) => {
     e.preventDefault();
     setShowAlert(true);
 
-
-
     try {
-      const response = await axios.post(REGISTER_URL, JSON.stringify({name, email, pass}), {
+      const response = await axios.post(REGISTER_URL, {
+        email: email,
+        password: pass,
+        username: name,
+        artist: artistChecked,
+        genre: genre,
+      }, {
         headers: {
           'Content-Type': 'application/json',
         }
@@ -45,10 +53,12 @@ function Register(props) {
       setEmail('');
       setPass('');
       setName('');
-      setArt(false);
-     
+      setGenre('');
+      setArtistChecked(false);
+      document.getElementById('artist-checkbox').checked = false;
 
     } catch (err) {
+
       if(!err?.response){
         setErr('No server response.');
       } else if(err?.response === 400){
@@ -59,7 +69,7 @@ function Register(props) {
         setErr('Registration failed.');
       }
       errRef.current.focus();
-    }    
+    }
     setTimeout(() => {
       setShowAlert(false);
     }, 5000);
@@ -67,15 +77,15 @@ function Register(props) {
     setTimeout(() => {
       e.target.submit();
     }, 5000);
-    
+
   }
 
   return (
-   
 
-    <div className="form-container">    
-      {showAlert && (<div className="alert">Registration succesful, redirecting to login page</div>)}
+    <div className="form-container">
+      {showAlert && (<div className="alert">Registration successful, redirecting to login page</div>)}
       <p ref={errRef} className={err ? "errmessage" : "offscreen"} aria-live='assertive'>{err}</p>
+
       <form className="registration-form" onSubmit={handleSubmit}>
         <p className="formHeader">Soundio</p>
 
@@ -84,8 +94,10 @@ function Register(props) {
         <input className="input" value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="email" id="email" name="email" autoComplete="given-name" required/>
 
         <input className="input" value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*********" id="password" name="password" autoComplete="off" required/>
-        
-        <FormControlLabel control={<Checkbox />} label="Register as an artist?" className="button" value={art} onChange={(e) => setArt(e.target.value)}/>
+
+        <FormControlLabel control={<Checkbox />} id="artist-checkbox" label="Register as an artist?" className="button" onChange={handleChange}/>
+
+        <input className="input" style={{display: 'none'}} value={genre} onChange={(e) => setGenre(e.target.value)} type="genre" placeholder="Genre" id="genre" name="Genre" required/>
 
         <button className="button">
           Register

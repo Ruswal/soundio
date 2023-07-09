b4a72d65b5aa9aa229c330286ccd13fbb1cba118
Raj Wadia <wadiaraj7@gmail.com>
2023-07-09 15:29:58 -0600

    homepage css fixed

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -1,11 +1,22 @@
-import React, { ReactDOM } from "react";
+import { default as React, ReactDOM, useContext, useEffect, useRef, useState } from "react";
+import { Link, Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import axios from '../api/axios.js';
+import AuthContext from "../context/AuthProvider.jsx";
 
+import "./homepage.css";
+
+import Header from '../components/Header';
+import MusicGrid from '../components/MusicGrid';
+import Navbar from '../components/NavBar';
+import Sidebar from '../components/Sidebar';
 
 const Homepage = () => {
 
   return(
-    <div>
-      <p>homepage</p>
+    <div className='homepage-container'>
+      <Header/>
+      <Sidebar/>
+      <MusicGrid/>
     </div>
   );
 

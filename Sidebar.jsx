a408bc686627ac4736d680f60cac61cf7f0e0efc
PingsongHuang <133330030+PingsongHuang@users.noreply.github.com>
2023-07-13 07:30:07 +0900

    Add files via upload
    observer model for the visualizer and keep track of the playlist

---
+++ Sidebar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Sidebar = () => (
+  <div className="sidebar">
+    <ul>
+    <li>Recommendation</li>
+      <li>My Music</li>
+      <li>Music List</li>
+    </ul>
+  </div>
+);
+
+export default Sidebar;

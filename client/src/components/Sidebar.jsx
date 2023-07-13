33021c6d4f408571b7f652ae16c569d2d02a7e5b
Raj Wadia <wadiaraj7@gmail.com>
2023-07-13 12:04:35 -0600

    added sidebar functionality

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,20 +1,50 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import axios from '../api/axios';
+import AuthContext from '../context/AuthProvider';
 import './style/Sidebar.css';
 
 const Sidebar = () => {
 
-  const handleCreatePlaylist = () => {
+  const POST_URL = 'http://localhost:3001/create-playlist';
 
+  const authContext = useContext(AuthContext);
+
+  const handleCreatePlaylist = async(e) => {
+
+    const fragment = document.createDocumentFragment();
+
+    const div = fragment.
+
+    document.getElementById('user-playlists').appendChild;
+
+    /* try{
+      const response = await axios.post(POST_URL, {
+        uid: USER_ID,
+        create_by: USER_NAME,
+        playlist_name: playlist_name,
+      }, {
+        headers:{
+          'Content-Type': 'application/json',
+          withCredentials: false,
+        }
+      })
+    }catch(err){
+      console.log(err);
+    } */
   }
 
   return(
     <div className="sidebar-container">
       <div className='sidebar'>
-        <ul>
-          <li>Home</li>
-          <li>Liked Music</li>
-          <li>play 1</li>
-        </ul>
+
+          <div>Home</div>
+          <div>Liked Music</div>
+          <div>play 1</div>
+
+          <div className='user-playlist-container'>
+            <div id='user-playlists' className='user-playlists'>Your Playlist</div>
+          </div>
+
         <div className='button createPlaylist' onClick={handleCreatePlaylist}>Create Playlist</div>
       </div>
     </div>

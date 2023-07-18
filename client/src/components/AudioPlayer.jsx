cd7bc84d2053f2a3b5947ffc06532f31d5d59517
Ruswal <andrew.petrie306@gmail.com>
2023-07-18 00:43:21 -0600

    New Branch + New Files

---
+++ client/src/components/AudioPlayer.jsx
@@ -0,0 +1,8 @@
+const AudioPlayer = () => {
+    return (
+      <div className="audio-player">
+        <div className="inner">Audio player content</div>
+      </div>
+    );
+  };
+  export default AudioPlayer;

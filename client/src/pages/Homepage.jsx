5b70f2e26f07bc294b06f44a02644b2b5fb1cdfd
Raj Wadia <wadiaraj7@gmail.com>
2023-07-19 17:28:58 -0600

    merged the sidebar component into the homepage

--- client/src/pages/Homepage.jsx
+++ client/src/pages/Homepage.jsx
@@ -7,23 +7,80 @@ import "./style/homepage.css";
 
 import Header from '../components/Header';
 import MusicGrid from '../components/MusicGrid';
-import Sidebar from '../components/Sidebar';
+import FileUploadPage from './artist-studio.jsx';
 
 const Homepage = () => {
 
-  const authContext = useContext(AuthContext);
+   const POST_URL = 'http://localhost:3001/create-playlist';
+  const [component, setComponent] = useState('');
 
-  console.log(authContext.data);
+  const authContext = useContext(AuthContext);
+  const USER_ID = authContext.data[0].ID;
+  const USER_NAME = authContext.data[0].username;
+
+  const changeElement = (e) => {
+    console.log(e);
+    setComponent(e);
+  }
+
+  const handleCreatePlaylist = async(e) => {
+
+
+    // TODO: uncomment the following snippet to send a request to the server.
+    try{
+      const response = await axios.post(POST_URL, {
+        uid: USER_ID,
+        create_by: USER_NAME,
+        playlist_name: playlist_name,
+        created_on: new Date(),
+      }, {
+        headers:{
+          'Content-Type': 'application/json',
+          withCredentials: false,
+        }
+      })
+    }catch(err){
+      console.log(err);
+    }
+  }
+
+  // console.log(authContext.data);
 
   return(
     <div className='homepage-container'>
       <Header/>
       <div className='homepage'>
         <div className='sidebar-holder'>
-          <Sidebar/>
+          <div className="sidebar-container">
+            <div className='sidebar'>
+
+                <div id='Discover' onClick = {(e)=> {changeElement(e.target.id)}}>Home</div>
+                <div>Liked Music</div>
+
+                {
+                  authContext.data[0].isArtist ? (
+                    <div id='FileUploadPage' onClick = {(e) => {changeElement(e.target.id)}}>
+                      Artist Studio
+                    </div>
+                  ) : <> </>
+                }
+
+                <div className='user-playlist-container'>
+                  <div id='user-playlists' className='user-playlists'>Your Playlists</div>
+                </div>
+                <div>
+                  <Link to='../pages/Audio.jsx'> audio </Link>
+                </div>
+
+              <div className='button createPlaylist' onClick={handleCreatePlaylist}>Create Playlist</div>
+            </div>
+          </div>
         </div>
         <div className='main-holder'>
-          <MusicGrid/>
+          {
+            component == 'FileUploadPage' ? <FileUploadPage/> :
+            component == 'Discover' ? <MusicGrid/> : <> </>
+          }
         </div>
       </div>
     </div>

4b5179de2388f8dd5955536c328037ac254196c5
Ruswal <andrew.petrie306@gmail.com>
2023-07-19 20:58:45 -0600

    update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,7 +5,8 @@ import './style/Header.css';
 
 const Header = () => (
   <header className="header">
-    <div className="logo">Soundio</div>
+    {/* <div className="logo">Soundio</div>  */}
+    <img className="logo" src="New_Logo_Soundio.png" alt="Logo Should be here" width="200" height="100"></img>
     <input type="text" placeholder="Search" className="search" />
     <Logout />
   </header>

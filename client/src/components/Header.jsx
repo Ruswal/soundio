baf0067dc0cd9ed4041d1b66bf842fa36b178f6b
Raj Wadia <wadiaraj7@gmail.com>
2023-07-21 16:09:46 -0600

    added username on the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,19 +1,28 @@
 import React from 'react';
 import Logo from '../assets/Logo.png';
 import Logout from './Logout';
+import UserInfo from './UserInfo';
 import './style/Header.css';
 
 
+const Header = () => {
+  return(
+    <header className="header">
 
-const Header = () => (
-  <header className="header">
-    {/* <div className="logo">Soundio</div>  */}
-    <div className='logo-container'>
-      <img className="logo" src={Logo} alt="Logo Should be here"></img>
-    </div>
-    <input type="text" placeholder="Search" className="search" />
-    <Logout />
-  </header>
-);
+      <div className='logo-container'>
+        <img className="logo" src={Logo} alt="Logo Should be here"></img>
+      </div>
+
+      <div className='searchbar-container'>
+        <input type="text" placeholder="Search" className="search" />
+      </div>
+
+      <div className='logout-container'>
+        <UserInfo/>
+        <Logout className='logout-element'/>
+      </div>
+    </header>
+  )
+};
 
 export default Header;

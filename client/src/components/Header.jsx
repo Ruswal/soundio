5e8a8c231946bffe754c4eebb3a81be3c113052c
TomiwaOgun <ogunleyetomiwa0@gmail.com>
2023-07-21 13:10:05 -0600

    music grid update

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Logo from '../assets/Logo.png';
 import Logout from './Logout';
 import './style/Header.css';
-import Logo from './Logo.png';
+
 
 
 const Header = () => (
--- client/src/components/MusicGrid.jsx
+++ client/src/components/MusicGrid.jsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import tracks from '../pages/data/tracks';
+import React, { useState, useEffect, useRef } from "react";
+import { tracks } from "../pages/data/tracks";
+import {
+  BiPlayCircle,
+  BiPauseCircle,
+  BiSkipPreviousCircle,
+  BiSkipNextCircle,
+} from "react-icons/bi";
 
 const MusicGrid = () => {
-  const musicList = tracks;  // This can be your actual data
+  const musicList = tracks; // This can be your actual data
 
-  const handlePlay = (url) => {
-    const audioPlayer = new Audio(url);
-    audioPlayer.play();
+  const [audioPlayers, setAudioPlayers] = useState([]);
+
+  useEffect(() => {
+    // Create audio players for each music item
+    const players = musicList.map((music) => new Audio(music.src));
+    setAudioPlayers(players);
+
+    return () => {
+      // Clean up audio players on unmount
+      players.forEach((player) => player.pause());
+    };
+  }, [musicList]);
+
+  const handlePlayPause = (index) => {
+    const newAudioPlayers = [...audioPlayers];
+    const audioPlayer = newAudioPlayers[index];
+
+    if (audioPlayer.paused) {
+      audioPlayer.play();
+    } else {
+      audioPlayer.pause();
+    }
+
+    setAudioPlayers(newAudioPlayers);
   };
 
   return (
-    <div className="music-grid">
-      {musicList.map((music, index) => (
-        <div className="music-item" key={index}>
-          <div className="music-name">{music.title}</div>
-          <div className="music-name">{music.author}</div>
-          <button onClick={() => handlePlay(music.src)}>Play</button>
-        </div>
-      ))}
+    <div>
+      <div className="music-grid">
+        {musicList.map((music, index) => (
+          <div className="music-item" key={index}>
+            <h2 className="music-name">{music.title}</h2>
+            <h4 className="music-name">{music.author}</h4>
+            {audioPlayers[index]?.paused ? (
+              <BiPlayCircle
+                color="#ff5722"
+                size={70}
+                className="icons"
+                onClick={() => handlePlayPause(index)}
+              />
+            ) : (
+              <BiPauseCircle
+                color="#ff5722"
+                size={70}
+                className="icons"
+                onClick={() => handlePlayPause(index)}
+              />
+            )}
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default MusicGrid;

e86279663046c84bed9b79d2df3ee2bd748e99b6
Raj Wadia <wadiaraj7@gmail.com>
2023-07-21 15:47:15 -0600

    logo resized

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -8,7 +8,9 @@ import './style/Header.css';
 const Header = () => (
   <header className="header">
     {/* <div className="logo">Soundio</div>  */}
-    <img className="logo" src={Logo} alt="Logo Should be here" width="200" height="100"></img>
+    <div className='logo-container'>
+      <img className="logo" src={Logo} alt="Logo Should be here"></img>
+    </div>
     <input type="text" placeholder="Search" className="search" />
     <Logout />
   </header>

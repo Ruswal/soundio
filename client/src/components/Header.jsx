75e7353027c7e71274a52498a5fba0ad99ca3f85
Raj Wadia <wadiaraj7@gmail.com>
2023-07-20 21:46:08 -0600

    added logo to the header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Logo from '../assets/Logo.png';
 import Logout from './Logout';
 import './style/Header.css';
 
@@ -6,7 +7,7 @@ import './style/Header.css';
 const Header = () => (
   <header className="header">
     {/* <div className="logo">Soundio</div>  */}
-    <img className="logo" src="./Logo.png" alt="Logo Should be here" width="200" height="100"></img>
+    <img className="logo" src={Logo} alt="Logo Should be here" width="200" height="100"></img>
     <input type="text" placeholder="Search" className="search" />
     <Logout />
   </header>

dfd5c13dbb91526e80c2f512cb6747c1dd9c93c0
Raj Wadia <wadiaraj7@gmail.com>
2023-07-28 22:53:23 -0600

    minor bug fix

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,11 +4,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthContextProvider } from './context/AuthProvider';
 
-var start = performance.now();
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AuthContextProvider>
     <App />
   </AuthContextProvider>
 );
- var timeTaken = performance.now() - start;
- console.log('Time taken for the app to render: ' + timeTaken + 'ms.')
